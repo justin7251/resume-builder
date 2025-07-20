5af6a2ca7d889209b527b90efe509d472ec6e0f6
justin725 <myjustinleung19@hotmail.com>
2025-07-20 12:01:23 +0100

    Reapply "Merge pull request #2 from justin7251/feature/multi-page-resume"
    This reverts commit 5c6b4786af53fc5440df6d2d079d9d2268239934.

---
+++ src/lib/components/ResumePage.svelte
@@ -0,0 +1,20 @@
+<div class="resume-page" style="break-after: page;">
+  <slot></slot>
+</div>
+
+<style>
+  .resume-page {
+    width: 210mm;
+    height: 297mm;
+    padding: 20mm;
+    box-sizing: border-box;
+    background: white;
+    box-shadow: 0 0 5px rgba(0,0,0,0.1);
+    margin-bottom: 1cm;
+  }
+
+  .printing .resume-page {
+    box-shadow: none;
+    margin: 0;
+  }
+</style>
